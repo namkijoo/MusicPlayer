38e41cc06e7abe3b94b8e4d185196b274ff70eee
namkijoo <rlwn5904@naver.com>
2024-10-18 16:09:55 +0900

    Feat:검색 리스트 구현

--- src/components/Search/MusicSearch.jsx
+++ src/components/Search/MusicSearch.jsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 import Login from '../Login/Login';
-import { FaSearch } from 'react-icons/fa';
+import { FaPlay, FaPlus, FaSearch } from 'react-icons/fa';
 import { useState } from 'react';
 import { getSearchMusicList } from '../../api/MusicApi/music.api';
 import { useQuery } from '@tanstack/react-query';
+import { BiPlay, BiPlus } from 'react-icons/bi';
 
 function MusicSearch() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -18,8 +19,14 @@ function MusicSearch() {
     refetch();
   };
 
+  const handleEnter = (e) => {
+    if (e.key === 'Enter') {
+      handleSearchClick();
+    }
+  };
+
   const { data = [], refetch } = useQuery({
-    queryKey: ['getSearchMusicList', searchTerm],
+    queryKey: ['getSearchMusicList'],
     queryFn: () => getSearchMusicList(searchTerm),
     enabled: fetchData,
     refetchOnWindowFocus: true,
@@ -31,15 +38,31 @@ function MusicSearch() {
       <SearchWrapper>
         <FaSearch onClick={handleSearchClick} />
         <span>검색</span>
-        <input onChange={onChangeSearch} value={searchTerm} />
+        <input onChange={onChangeSearch} value={searchTerm} onKeyDown={handleEnter} />
       </SearchWrapper>
       <Line />
-
-      {data.length > 0 ? (
-        data.map((element, index) => <div key={index}>{element.snippet.title}</div>)
-      ) : (
-        <ResultImg src="/search.png" />
-      )}
+      <MusicListWrapper>
+        {data.length > 0 ? (
+          data.map((element, index) => (
+            <MusicList key={index}>
+              <MusicListLeft>
+                <img src={element.snippet.thumbnails.medium.url} />
+                <span>
+                  {element.snippet.title.length > 40
+                    ? element.snippet.title.slice(0, 40) + '...'
+                    : element.snippet.title}
+                </span>
+              </MusicListLeft>
+              <MusicListRight>
+                <FaPlay />
+                <FaPlus />
+              </MusicListRight>
+            </MusicList>
+          ))
+        ) : (
+          <ResultImg src="/search.png" />
+        )}
+      </MusicListWrapper>
     </Container>
   );
 }
@@ -66,6 +89,7 @@ const SearchWrapper = styled.div`
     right: 10px;
     font-size: 17px;
     color: #383737;
+    cursor: pointer;
   }
   > input {
     border-radius: 20px;
@@ -88,8 +112,54 @@ const Line = styled.hr`
   margin-top: 40px;
   border-top: 0.1px dotted gray;
 `;
+
 const ResultImg = styled.img`
   margin-top: 180px;
+  margin-left: 50%;
+  transform: translateX(-50%);
+`;
+
+const MusicListWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const MusicList = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 15px 0;
+`;
+
+const MusicListLeft = styled.div`
+  display: flex;
+  align-items: center;
+  > img {
+    width: 80px;
+  }
+  > span {
+    color: lightgray;
+    margin-left: 10px;
+  }
+`;
+
+const MusicListRight = styled.div`
+  display: flex;
+  color: gray;
+
+  :nth-child(1) {
+    cursor: pointer;
+  }
+  :nth-child(2) {
+    margin: 0 20px;
+    cursor: pointer;
+  }
 `;
 
 export default MusicSearch;
