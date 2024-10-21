3374e5d842f24578a133121e925ca9b22ebdc02b
namkijoo <rlwn5904@naver.com>
2024-10-21 17:47:53 +0900

    Chore:로그인 시에 로그아웃으로 글자 변경

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,39 @@
 import { useGoogleLogin } from '@react-oauth/google';
+import { useEffect, useState } from 'react';
 import { CiUser } from 'react-icons/ci';
 import styled from 'styled-components';
 
 const VITE_YOUTUBE_SCOPE = import.meta.env.VITE_YOUTUBE_SCOPE;
 
 function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+  }, []);
+
   const login = useGoogleLogin({
     flow: 'implicit', // 'auth-code' 대신 'implicit' 플로우 사용
     onSuccess: (tokenResponse) => {
       localStorage.setItem('token', tokenResponse.access_token);
+      setIsLoggedIn(true);
       console.log(tokenResponse.access_token);
     },
     onError: (errorResponse) => console.log(errorResponse),
     scope: VITE_YOUTUBE_SCOPE,
   });
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    alert('로그아웃 되었습니다. ');
+    setIsLoggedIn(false);
+  };
   return (
     <TopMenuWrapper>
-      <LoginWrapper onClick={login}>
+      <LoginWrapper onClick={isLoggedIn ? logout : login}>
         <CiUser />
-        <span>로그인</span>
+        <span>{isLoggedIn ? '로그아웃' : '로그인'}</span>
       </LoginWrapper>
     </TopMenuWrapper>
   );
