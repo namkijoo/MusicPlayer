edaf627a33ebc7fbbf425058de9512abe3902a76
namkijoo <rlwn5904@naver.com>
2024-12-23 02:17:29 +0900

    Fix:설정파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // 빌드 파일이 생성될 폴더
+  },
+  base: './', // 정적 파일을 상대 경로로 설정
+});
