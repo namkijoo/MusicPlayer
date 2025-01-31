dc543ab8057ba056c3cd321e4a44edbfbd10d897
namkijoo <rlwn5904@naver.com>
2025-02-01 01:52:51 +0900

    Feat:pwa설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,58 @@ export default defineConfig({
       exportAsDefault: false,
       svgrOptions: {
         icon: true,
+      },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: '음플',
+        short_name: 'Music Player',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './icons/apple-touch-icon-57x57.png',
+            sizes: '57x57',
+            type: 'image/png',
+          },
+          {
+            src: './icons/apple-touch-icon-60x60.png',
+            sizes: '60x60',
+            type: 'image/png',
+          },
+          {
+            src: './icons/apple-touch-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: './icons/apple-touch-icon-76x76.png',
+            sizes: '76x76',
+            type: 'image/png',
+          },
+          {
+            src: './icons/apple-touch-icon-114x114.png',
+            sizes: '114x114',
+            type: 'image/png',
+          },
+          {
+            src: './icons/apple-touch-icon-120x120.png',
+            sizes: '120x120',
+            type: 'image/png',
+          },
+
+          {
+            src: './icons/apple-touch-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: './icons/apple-touch-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+        ],
       },
     }),
   ],
